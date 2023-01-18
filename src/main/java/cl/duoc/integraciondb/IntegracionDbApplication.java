fd1d81e83997c3763d603dfb882b394d65d3a9e8
jazocar <jazocar@gasco.cl>
2023-01-18 16:01:55 -0300

    first commit

---
+++ src/main/java/cl/duoc/integraciondb/IntegracionDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.integraciondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/integraciondb/IntegracionDbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.integraciondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracionDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
